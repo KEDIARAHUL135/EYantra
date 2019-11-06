0785d4d84a5e6b1b27aa347956f3c8e448606284
Rahul Kedia <kediarahul1011@gmail.com>
2019-11-06 17:37:04 +0530

    Complete Task1.1

--- Task 1/Task1.2/Codes/main.py
+++ Task 1/Task1.2/Codes/main.py
@@ -25,6 +25,15 @@ images_folder_path = os.path.abspath(os.path.join('..', 'Images'))
 generated_folder_path = os.path.abspath(os.path.join('..', 'Generated'))
 
 
+######################################################################
+## Self made Functions   -- made by Kedia
+######################################################################
+def CallbackFunction(var):
+    return
+
+######################################################################
+## End of Self made Functions   -- made by Kedia
+######################################################################
 
 
 ############################################
@@ -40,10 +49,133 @@ def process(ip_image):
     sector_image = np.ones(ip_image.shape[:2],np.uint8)*255
     ## check value is white or not
     print(sector_image[0,0])
-    ## Your Code goes here
+
+    #=================================
+    # Code for finding red and green dots.. uncomment to apply
+    '''cv2.namedWindow("Trackbar")
+    cv2.createTrackbar("LR_1", "Trackbar", 0, 255, CallbackFunction)
+    cv2.createTrackbar("LR_2", "Trackbar", 0, 255, CallbackFunction)
+    cv2.createTrackbar("LR_3", "Trackbar", 0, 255, CallbackFunction)
+    cv2.createTrackbar("UR_1", "Trackbar", 0, 255, CallbackFunction)
+    cv2.createTrackbar("UR_2", "Trackbar", 0, 255, CallbackFunction)
+    cv2.createTrackbar("UR_3", "Trackbar", 0, 255, CallbackFunction)
+    hsv = cv2.cvtColor(ip_image, cv2.COLOR_BGR2HSV)
+
+    while(cv2.waitKey(1) != 27):
+        LR_1 = cv2.getTrackbarPos("LR_1", "Trackbar")
+        LR_2 = cv2.getTrackbarPos("LR_2", "Trackbar")
+        LR_3 = cv2.getTrackbarPos("LR_3", "Trackbar")
+        UR_1 = cv2.getTrackbarPos("UR_1", "Trackbar")
+        UR_2 = cv2.getTrackbarPos("UR_2", "Trackbar")
+        UR_3 = cv2.getTrackbarPos("UR_3", "Trackbar")
+        LowerRange = np.array([LR_1, LR_2, LR_3])
+        UpperRange = np.array([UR_1, UR_2, UR_3])
+
+        Mask = cv2.inRange(hsv, LowerRange, UpperRange)
+
+        cv2.imshow("Masked", Mask)'''
+    # End of code for finding dots
+    #====================================
+    
+    hsv = cv2.cvtColor(ip_image, cv2.COLOR_BGR2HSV)
+    LowerRange = np.array([0, 4, 0])
+    UpperRange = np.array([0, 127, 255])
+
+    Mask = cv2.inRange(hsv, LowerRange, UpperRange)
+
+    Height, Width = Mask.shape[:2]
+    
+    # Up-Down check
+    FoundCircle = 0
+    for i in range(int(Height/2)-5):
+        if Mask[(int(Height/2)-i), int(Width/2)] != 0:
+            if FoundCircle == 0:
+                InnerCircleRadiusUp = i
+            FoundCircle = 1
+
+        if FoundCircle == 1:
+            if Mask[(int(Height/2)-i), int(Width/2)] == 0:            
+                FoundCircle = 2
+                OuterCircleRadiusUp = i
+                break
+
+    FoundCircle = 0
+    for i in range(int(Height/2)-5):
+        if Mask[(int(Height/2)+i), int(Width/2)] != 0:
+            if FoundCircle == 0:
+                InnerCircleRadiusDown = i
+            FoundCircle = 1
+
+        if FoundCircle == 1:
+            if Mask[(int(Height/2)+i), int(Width/2)] == 0:            
+                FoundCircle = 2
+                OuterCircleRadiusDown = i
+                break
+
+    # Left-Right check
+    FoundCircle = 0
+    for j in range(int(Width/2)-5):
+        if Mask[int(Height/2), (int(Width/2)-j)] != 0:
+            if FoundCircle == 0:
+                InnerCircleRadiusLeft = i
+            FoundCircle = 1
+
+        if FoundCircle == 1:
+            if Mask[int(Height/2), (int(Width/2)-j)] == 0:            
+                FoundCircle = 2
+                OuterCircleRadiusLeft = i
+                break
+
+    FoundCircle = 0
+    for j in range(int(Width/2)-5):
+        if Mask[int(Height/2), (int(Width/2)+j)] != 0:
+            if FoundCircle == 0:
+                InnerCircleRadiusRight = i
+            FoundCircle = 1
+
+        if FoundCircle == 1:
+            if Mask[int(Height/2), (int(Width/2)+j)] == 0:            
+                FoundCircle = 2
+                OuterCircleRadiusRight = i
+                break
+
+
+    List1 = [OuterCircleRadiusUp, OuterCircleRadiusDown, OuterCircleRadiusLeft, OuterCircleRadiusRight] 
+    List1.sort()
+    FinalRadiusOut = List1[1]
+
+    List2 = [InnerCircleRadiusUp, InnerCircleRadiusDown, InnerCircleRadiusLeft, InnerCircleRadiusRight] 
+    List2.sort()
+    FinalRadiusIn = List2[1]
+
+    
+
+        
+    ColourOfCircle = (0,255,0)
+    ip_image = cv2.circle(ip_image, (int(Height/2), int(Width/2)), FinalRadiusOut, ColourOfCircle, -1)
+    ip_image = cv2.circle(ip_image, (int(Height/2), int(Width/2)), FinalRadiusIn, (0,0,255), -1)
+
+    
+    cv2.imshow("Masked", Mask)
+    cv2.imshow("Input", ip_image)
+    
+    for i in range(Height):
+        for j in range(Width):
+            if (ip_image[i,j][0] == ColourOfCircle[0]) and (ip_image[i,j][1] == ColourOfCircle[1]) and (ip_image[i,j][2] == ColourOfCircle[2]):
+                sector_image[i,j] = Mask[i,j]
+
+
+    sector_image[:,:] = 255 - sector_image[:,:]
+    kernel = np.ones((3,3),np.uint8)
+    sector_image = cv2.erode(sector_image,kernel,iterations = 1)
+    sector_image = cv2.dilate(sector_image,kernel,iterations = 1)
+    sector_image[:,:] = 255 - sector_image[:,:]
+    
+   ## Your Code goes here
     ###########################
     cv2.imshow("window", sector_image)
     cv2.waitKey(0);
+    cv2.destroyAllWindows() # Remove it at the end
     return sector_image
 
 
